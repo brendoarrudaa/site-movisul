ccc90533f0cf25c1ab20f210d80dfb0eb0706025
Brendo Arruda <brendo.arruda@gmail.com>
2026-03-28 09:22:50 -0300

    feat

--- src/components/SearchBlogClient/index.js
+++ src/components/SearchBlogClient/index.js
@@ -3,17 +3,20 @@ import { InstantSearch } from 'react-instantsearch'
 
 import SearchBlog from 'components/SearchBlog'
 
-const searchClient = algoliasearch(
-  process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
-  process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_ONLY_KEY
-)
+const appId = process.env.NEXT_PUBLIC_ALGOLIA_APP_ID
+const searchKey = process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_ONLY_KEY
+const indexName = process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME
+
+const searchClient =
+  appId && searchKey ? algoliasearch(appId, searchKey) : null
 
 const SearchBlogClient = () => {
+  if (!searchClient || !indexName) {
+    return <p>Pesquisa indisponível no momento.</p>
+  }
+
   return (
-    <InstantSearch
-      indexName={process.env.NEXT_PUBLIC_ALGOLIA_INDEX_NAME}
-      searchClient={searchClient}
-    >
+    <InstantSearch indexName={indexName} searchClient={searchClient}>
       <SearchBlog />
     </InstantSearch>
   )
